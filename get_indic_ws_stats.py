b5437569d54a8d71cbc1f86cebfc94ce13830cbe
T Shrinivasan <tshrinivasan@gmail.com>
2018-10-05 09:06:09 +0000

    dded colors

--- get_indic_ws_stats.py
+++ get_indic_ws_stats.py
@@ -49,10 +49,20 @@ table_head = """
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<style type="text/css">
+.quality4 { background:#90ff90; }
+.quality3 { background:#ffe867; }
+.quality2 { background:#b0b0ff; }
+.quality1 { background:#ffa0a0; }
+.quality0 { background:#dddddd; }
+
+.withscans { background:#c0c0ff; }
+.naked { background:#ffa0a0; }
+.disamb { background:#dddddd; }
+</style>
 </head>
 <body>
 <div class="container">
- <div class="table-responsive">
 <table class="table table-striped table-bordered">
 <tr>
 <td></td>
@@ -84,6 +94,7 @@ for i in range(0,x):
         #fle.write (str(allrows[i]).replace('</td><td>',' || ').replace('<tr><td>','| ').replace('</td></tr>','')+'\n')
         #fle.write (str('|-')+'\n')
         fle1.write(str(allrows[i]))
-fle1.write("</table></div></div>")
+fle1.write("</table></div>")
 fle1.write("<p align='right'> This list is updated daily once. Last update was on  " + timestamp + " GMT <br/>")
+fle1.write("<p align='right'> Source : <a href = 'https://github.com/KaniyamFoundation/Indic_wikisource_stats'> https://github.com/KaniyamFoundation/Indic_wikisource_stats ")
 fle1.close()
